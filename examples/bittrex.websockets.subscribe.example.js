698a2591306f339864531bb8c1b4b1639e4ef1e7
Valery Baranov <valery@baranov.xyz>
2018-05-04 14:54:06 +0300

    Change example to get orderbook for all tickers

--- examples/bittrex.websockets.subscribe.example.js
+++ examples/bittrex.websockets.subscribe.example.js
@@ -1,40 +1,61 @@
+const bittrex = require("../node.bittrex.api");
+const fs = require("fs");
 
-const bittrex = require('../node.bittrex.api');
+async function getMarkets() {
+  return new Promise((res, rej) => {
+    bittrex.getmarketsummaries(function (response, err) {
+      if (err) rej(err);
+      if (!response.success) rej(response.message);
+      res(response.result.map(m => m["MarketName"]));
+    });
+  });
+}
 
-bittrex.options({
-  'verbose' : true,
-});
+async function main() {
+  const markets = await getMarkets();
+  
+  console.log("Connecting ....");
+  bittrex.websockets.client(function (client) {
+    // connected - you can do any one-off connection events here
+    //
+    // Note: Reoccuring events like listen() and subscribe() should be done
+    // in onConnect so that they are fired during a reconnection event.
+    console.log("Connected");
+  });
+  
+  bittrex.options({
+    "verbose": true
+  });
+  
+  bittrex.options({
+    verbose   : true,
+    websockets: {
+      onConnect: function () {
+        console.log("onConnect fired");
+        bittrex.websockets.subscribe(markets, function (data, client) {
+          if (data.M === "updateExchangeState") {
+            data.A.forEach(function (data_for) {
+              // console.log("Market Update for " + data_for.MarketName, data_for.Sells);
+              const fileName = `${data_for.MarketName}.log`.toLowerCase();
+              fs.appendFile(fileName, JSON.stringify(data_for), function (err) {
+                if (err) throw err;
+                // console.log(`Added data to ${fileName} file.`);
+              });
+              
+            });
+          }
+        });
+        
+        // bittrex.websockets.subscribe(["BTC-OMG"], function (data, client) {
+        //   if (data.M === "updateExchangeState") {
+        //     data.A.forEach(function (data_for) {
+        //       console.log("Market Update for " + data_for.MarketName);
+        //     });
+        //   }
+        // });
+      }
+    }
+  });
+}
 
-bittrex.options({
-  verbose: true,
-  websockets: {
-    onConnect: function() {
-      console.log('onConnect fired');
-      bittrex.websockets.subscribe(['BTC-ETH'], function(data, client) {
-        if (data.M === 'updateExchangeState') {
-          data.A.forEach(function(data_for) {
-            console.log('Market Update for '+ data_for.MarketName);
-          });
-        }
-      });
-      
-      
-      bittrex.websockets.subscribe(['BTC-OMG'], function(data, client) {
-        if (data.M === 'updateExchangeState') {
-          data.A.forEach(function(data_for) {
-            console.log('Market Update for '+ data_for.MarketName);
-          });
-        }
-      });
-    },
-  },
-});
-
-console.log('Connecting ....');
-bittrex.websockets.client(function(client) {
-  // connected - you can do any one-off connection events here
-  //
-  // Note: Reoccuring events like listen() and subscribe() should be done
-  // in onConnect so that they are fired during a reconnection event.
-  console.log('Connected');
-});
+main().then(() => console.log("done")).catch(error => console.error(error));
